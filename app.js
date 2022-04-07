4e7e53524a8bb77542fbb887b349982d9cace506
DB GUPTA <deshbandhugupta4@gmail.com>
2022-04-07 14:25:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+Filevalidation = () => {
+    const fi = document.getElementById('file');
+    if (fi.files.length > 0) {
+        for (const i = 0; i <= fi.files.length - 1; i++) {
+
+            const fsize = fi.files.item(i).size;
+            const file = Math.round((fsize / 1024));
+        
+            if (file >= 1096) {
+                document.getElementById('size').innerHTML = '<b>'
+                + file + '</b> KB';
+                alert(
+                  "File too Big, please select a file less than 1mb");
+            } else {
+                document.getElementById('size').innerHTML = '<b>'
+                + file + '</b> KB';
+            }
+        }
+    }   
+}
+
+
+function onlyNumberKey(evt) {
+          
+    
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
